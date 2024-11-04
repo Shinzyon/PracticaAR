34d2cad9e3809d7c51d44f9da080091d4b79d5fd
Shinzyon <ivandesarrollojuegos@gmail.com>
2024-11-04 11:53:48 +0100

    V0.0

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OnMainMenu;
+    public event Action OnItemsMenu;
+    public event Action OnARPosition;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        MainMenu();
+    }
+    public void MainMenu()
+    {
+        OnMainMenu?.Invoke();
+        Debug.Log("Main Menu Activated");
+    }
+    public void ItemsMenu()
+    {
+        OnItemsMenu?.Invoke();
+        Debug.Log("Items Menu Activated");
+    }
+    public void ARPosition()
+    {
+        OnARPosition?.Invoke();
+        Debug.Log("AR Position Activated");
+    }
+
+    public void CloseAPP()
+    {
+        Application.Quit();
+    }
+}
